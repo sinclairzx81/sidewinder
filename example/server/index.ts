b6d9e11a5728b359efe406da000058214175d718
sinclairzx81 <haydn.developer@gmail.com>
2022-02-06 01:44:21 +0900

    updates

--- example/server/index.ts
+++ example/server/index.ts
@@ -1,14 +1,14 @@
 import { Host, WebSocketService } from '@sidewinder/server'
-import { Contract }         from '../shared/index'
 import { WebSocketClient }        from '@sidewinder/client'
-import cors                 from 'cors'
+import { Contract }               from '../shared/index'
+import cors                       from 'cors'
 
 const service = new WebSocketService(Contract)
 service.method('add', (clientId, a, b) => a + b)
 service.method('sub', (clientId, a, b) => a - b)
 service.method('mul', (clientId, a, b) => a * b)
 service.method('div', (clientId, a, b) => a / b)
- 
+
 const host = new Host()
 host.use(cors())
 host.use('/math', service) 
