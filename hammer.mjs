b1cd9ef3fa6c2eb960631245c1b9a2c99180620f
sinclairzx81 <haydn.developer@gmail.com>
2022-02-06 01:32:44 +0900

    updates

--- hammer.mjs
+++ hammer.mjs
@@ -32,7 +32,7 @@ export async function test() {
 // Build
 // -------------------------------------------------------------
 
-const VERSION = '0.8.1'
+const VERSION = '0.8.2'
 
 export async function build(target = 'target/build') {
     await clean()
