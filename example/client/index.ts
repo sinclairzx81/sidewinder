36144366a5bd18beca7d3f970ef934e37de5bbd3
sinclairzx81 <haydn.developer@gmail.com>
2022-02-07 00:45:06 +0900

    updates

--- example/client/index.ts
+++ example/client/index.ts
@@ -4,14 +4,11 @@ import { Contract }         from '../shared/index'
 const client  = new WebSocketClient(Contract, 'ws://localhost:5001/math')
 const result = await client.call('add', 1, 2)
 const buffer = await client.call('buf', new Uint8Array(10))
-
-for(let i = 0; i < 1000; i++) {
-    const query  = await client.call('query', { name: { $eq: 'dave' } })
-    console.log('query', query)
-}
+const query  = await client.call('query', { name: { $eq: 'dave' } })
 
 console.log('result', result)
 console.log('buffer', buffer)
+console.log('query', query)
 
 
  
