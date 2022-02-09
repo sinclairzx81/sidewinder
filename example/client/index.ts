fed70bff475f1ee2b1d26c831c5e639584f0c67a
sinclairzx81 <haydn.developer@gmail.com>
2022-02-09 16:36:37 +0900

    updates

--- example/client/index.ts
+++ example/client/index.ts
@@ -1,9 +1,9 @@
-import { WebClient, WebProxy } from '@sidewinder/client'
-import { Contract }         from '../shared/index'
+import { WebClient } from '@sidewinder/client'
+import { Contract }  from '../shared/index'
 
-async function test() {
+async function clientTest() {
     const client = new WebClient(Contract, 'http://localhost:5001/math')
     const result = await client.call('add', 1, undefined)
     console.log('result', result)
 }
-test()
+clientTest()
