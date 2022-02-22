ead85b4b679d1b7f9cb8d8c895ba624a3ef3e179
sinclairzx81 <haydn.developer@gmail.com>
2022-02-23 00:37:08 +0900

    updates

--- tests/redis/database/database.ts
+++ tests/redis/database/database.ts
@@ -1,4 +1,47 @@
-import { RedisDatabase } from '@sidewinder/redis'
-import { resolveRedis } from '../resolve'
+import { RedisDatabase, RedisArray, RedisMap, RedisSet } from '@sidewinder/redis'
+import { resolveDatabase } from '../resolve'
+import { Assert } from '../../assert/index'
 
-describe('redis/RedisDatabase', () => {})
+describe('redis/RedisDatabase', () => {
+  it('should return known array', () => {
+    const database = resolveDatabase()
+    const array = database.array('vectors')
+    Assert.isInstanceOf(array, RedisArray)
+  })
+
+  it('should return known set', () => {
+    const database = resolveDatabase()
+    const set = database.set('vectors')
+    Assert.isInstanceOf(set, RedisSet)
+  })
+
+  it('should return known map', () => {
+    const database = resolveDatabase()
+    const map = database.map('vectors')
+    Assert.isInstanceOf(map, RedisMap)
+  })
+
+  // -------------------------------------------------
+  // Asserts
+  // -------------------------------------------------
+
+  it('should throw on unknown array', () => {
+    const database = resolveDatabase()
+    // @ts-ignore
+    Assert.throws(() => database.array('unknown'))
+    
+  })
+
+  it('should throw on unknown set', () => {
+    const database = resolveDatabase()
+    // @ts-ignore
+    Assert.throws(() => database.set('unknown'))
+    
+  })
+
+  it('should throw on unknown map', () => {
+    const database = resolveDatabase()
+    // @ts-ignore
+    Assert.throws(() => database.map('unknown'))
+  })
+})
--- tests/redis/resolve.ts
+++ tests/redis/resolve.ts
@@ -13,7 +13,7 @@ export function resolveDatabase() {
     maps: {
       vectors: Vector,
     },
-    set: {
+    sets: {
       vectors: Vector,
     },
   })
