1e5232f2df8a3ee6e4099f24f4badbf00e92700d
sinclairzx81 <haydn.developer@gmail.com>
2022-03-02 17:46:17 +0900

    updates

--- example/basic/server/index.ts
+++ example/basic/server/index.ts
@@ -1,35 +1,70 @@
 import { Type, Host, WebSocketService } from '@sidewinder/server'
 import { WebSocketClient } from '@sidewinder/client'
-import { Contract } from '../shared/index'
+import { MathServiceContract } from '../shared/index'
 import cors from 'cors'
 
-const service = new WebSocketService(Contract, Type.Object({
+// -----------------------------------------------------------------
+// Service
+// -----------------------------------------------------------------
+
+export const MathServiceContext = Type.Object({
     clientId: Type.String(),
     name:     Type.String(),
     roles:    Type.Array(Type.String())
-}))
-service.event('authorize', (clientId, request) => {
-    return { clientId, name: 'dave', roles: [] }
 })
-service.event('connect', (context) => console.log('server:connect', context))
-service.event('close',   (context) => console.log('server:close',   context))
-service.method('add',    (context, a, b) => a + b)
-service.method('sub',    (context, a, b) => a - b)
-service.method('mul',    (context, a, b) => a * b)
-service.method('div',    (context, a, b) => a / b)
+
+export class MathService extends WebSocketService<typeof MathServiceContract, typeof MathServiceContext> {
+
+    constructor() {
+        super(MathServiceContract, MathServiceContext)
+    }
+
+    // -----------------------------------------------------------------
+    // Events
+    // -----------------------------------------------------------------
+
+    on_authorize = this.event('authorize', (clientId, request) => {
+        console.log('server:authorize', clientId, request.query)
+        return { clientId, name: 'dave', roles: [] }
+    })
+
+    on_connect = this.event('connect', (context) => {
+        console.log('server:connect', context)
+    })
+
+    on_close = this.event('close', (context) => {
+        console.log('server:close', context)
+    })
+
+    // -----------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------
+
+    add = this.method('add', (context, a, b) => a + b)
+    sub = this.method('sub', (context, a, b) => a - b)
+    mul = this.method('mul', (context, a, b) => a * b)
+    div = this.method('div', (context, a, b) => a / b)
+}
+
+// -----------------------------------------------------------------
+// Host
+// -----------------------------------------------------------------
 
 const host = new Host()
 host.use(cors())
-host.use('/math', service)
-host.listen(5001).then(() => console.log('service running on port 5001'))
+host.use('/math', new MathService())
+host.listen(5001)
+
+// -----------------------------------------------------------------
+// Client Test
+// -----------------------------------------------------------------
 
 async function clientTest() {
-    const client = new WebSocketClient(Contract, 'ws://localhost:5001/math?token=123')
-    const result0 = await client.call('add', 1, 2)
-    const result1 = await client.call('sub', 1, 2)
-    const result2 = await client.call('mul', 1, 2)
-    const result3 = await client.call('div', 1, 2)
-    client.close()
-    console.log('client-results:', result0, result1, result2, result3)
+    const client = new WebSocketClient(MathServiceContract, 'ws://localhost:5001/math?token=<signed-token>')
+    const add = await client.call('add', 1, 2)
+    const sub = await client.call('sub', 1, 2)
+    const mul = await client.call('mul', 1, 2)
+    const div = await client.call('div', 1, 2)
+    console.log('client:', add, sub, mul, div)
 }
 clientTest().catch(error => console.log(error))
--- example/basic/shared/contract.ts
+++ example/basic/shared/contract.ts
@@ -1,12 +1,15 @@
 import { Type } from '@sidewinder/contract'
 
-export const Contract = Type.Contract({
-    format: 'json',
+export const MathServiceContract = Type.Contract({
+    format: 'msgpack',
     server: {
         add: Type.Function([Type.Number(), Type.Number()], Type.Number()),
         sub: Type.Function([Type.Number(), Type.Number()], Type.Number()),
         mul: Type.Function([Type.Number(), Type.Number()], Type.Number()),
         div: Type.Function([Type.Number(), Type.Number()], Type.Number()),
+    },
+    client: {
+        echo: Type.Function([Type.String()], Type.String())
     }
 })
 
