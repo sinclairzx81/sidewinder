492903e69b99186b3f8573595a97b6db3d038e66
sinclairzx81 <haydn.developer@gmail.com>
2022-03-02 17:48:05 +0900

    updates

--- example/basic/client/index.ts
+++ example/basic/client/index.ts
@@ -1,8 +1,8 @@
 import { WebSocketClient } from '@sidewinder/client'
-import { Contract }  from '../shared/index'
+import { MathServiceContract }  from '../shared/index'
 
 async function clientTest() {
-    const client = new WebSocketClient(Contract, 'ws://localhost:5001/math')
+    const client = new WebSocketClient(MathServiceContract, 'ws://localhost:5001/math')
     const result = await client.call('add', 1, 2)
     console.log('result:', result)
 }
