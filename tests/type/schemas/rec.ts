49f936abe41d0046d27b8146eafa389fb83a048d
sinclairzx81 <haydn.developer@gmail.com>
2022-04-26 21:39:38 +0900

    additional tests for recursive schemas

--- tests/type/schemas/rec.ts
+++ tests/type/schemas/rec.ts
@@ -18,23 +18,39 @@ describe('type/Rec', () => {
     })
   })
 
-  // https://github.com/ajv-validator/ajv/issues/1964
   it('Should validate wrapped recursive node type', () => {
-    // const Node = Type.Tuple([Type.Rec((Self) =>
-    //   Type.Object(
-    //     {
-    //       id: Type.String(),
-    //       nodes: Type.Array(Self),
-    //     }
-    //   ),
-    // )])
-    // console.log(JSON.stringify(Node, null, 2))
-    // ok(Node, [{
-    //   id: 'A',
-    //   nodes: [
-    //     { id: 'B', nodes: [] },
-    //     { id: 'C', nodes: [] },
-    //   ],
-    // }])
+    const Node = Type.Tuple([Type.Rec((Self) =>
+      Type.Object(
+        {
+          id: Type.String(),
+          nodes: Type.Array(Self),
+        }
+      ),
+    )])
+    ok(Node, [{
+      id: 'A',
+      nodes: [
+        { id: 'B', nodes: [] },
+        { id: 'C', nodes: [] },
+      ],
+    }])
+  })
+
+  it('Should not validate wrapped recursive node type with invalid id', () => {
+    const Node = Type.Tuple([Type.Rec((Self) =>
+      Type.Object(
+        {
+          id: Type.String(),
+          nodes: Type.Array(Self),
+        }
+      ),
+    )])
+    fail(Node, [{
+      id: 'A',
+      nodes: [
+        { id: 1, nodes: [] },
+        { id: 'C', nodes: [] },
+      ],
+    }])
   })
 })
