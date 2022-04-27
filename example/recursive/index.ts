f907f2a633d9402bd68582a9a35502b7470dcb9a
sinclairzx81 <haydn.developer@gmail.com>
2022-04-27 16:26:42 +0900

    keyof as union literal

--- example/recursive/index.ts
+++ example/recursive/index.ts
@@ -1,11 +1,79 @@
-import { Type } from '@sidewinder/type'
+// -----------------------------------------------------------------
+// This example tests recursive type inference and validation
+// through sidewinder services.
+// -----------------------------------------------------------------
 
-const T = Type.Object({
-    a: Type.String(),
-    b: Type.String(),
-    c: Type.String()
+import { Type, Host, WebService } from '@sidewinder/server'
+import { WebClient } from '@sidewinder/client'
+
+// -----------------------------------------------------------
+// Node
+// -----------------------------------------------------------
+
+const Node = Type.Tuple([
+    Type.Rec(Self => Type.Object({
+        id: Type.String(),
+        nodes: Type.Array(Self)
+    }))
+])
+
+// -----------------------------------------------------------
+// Contract
+// -----------------------------------------------------------
+
+const GraphContract = Type.Contract({
+    server: {
+        echo: Type.Function([Node], Node)
+    }
 })
 
-const K = Type.KeyOf(T)
+// -----------------------------------------------------------
+// Service
+// -----------------------------------------------------------
 
-console.log(K)
+export class GraphService extends WebService<typeof GraphContract> {
+    constructor() {
+        super(GraphContract)
+    }
+
+    public echo = super.method('echo', (context, node) => {
+        return node
+    })
+}
+
+// -----------------------------------------------------------
+// Host
+// -----------------------------------------------------------
+
+const host = new Host()
+
+host.use('/graph', new GraphService())
+
+host.listen(5000)
+
+// -----------------------------------------------------------
+// Client
+// -----------------------------------------------------------
+
+async function start() {
+
+    const client = new WebClient(GraphContract, 'http://localhost:5000/graph')
+
+    const result = await client.call('echo', [{
+        id: 'A',
+        nodes: [
+            { id: 'B', nodes: [{
+                id: 'A',
+                nodes: [
+                    { id: 'B', nodes: [] },
+                    { id: 'C', nodes: [] },
+                ]
+            }] },
+            { id: 'C', nodes: [] },
+        ]
+    }])
+    
+    console.log(JSON.stringify(result, null, 2)) 
+}
+
+start()
