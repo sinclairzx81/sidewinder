98d08c4f99d86240540ce0a7065154bf8818eed7
sinclairzx81 <haydn.developer@gmail.com>
2022-07-21 18:34:47 +0900

    updates

---
+++ example/mime/index.ts
@@ -0,0 +1,3 @@
+import { Mime } from '@sidewinder/mime'
+
+console.log(Mime.get('testing.mp4'))
--- tests/mime/mime.ts
+++ tests/mime/mime.ts
@@ -3,14 +3,19 @@ import { Mime } from '@sidewinder/mime'
 
 describe('mime/Mime', () => {
   it('should return expected mime types', () => {
-    Assert.equal(Mime.lookup('file.png'), 'image/png')
-    Assert.equal(Mime.lookup('file.mp4'), 'video/mp4')
-    Assert.equal(Mime.lookup('file.txt'), 'text/plain')
-    Assert.equal(Mime.lookup('file.json'), 'application/json')
-    Assert.equal(Mime.lookup('file.xml'), 'application/xml')
+    Assert.equal(Mime.get('file.png'), 'image/png')
+    Assert.equal(Mime.get('file.mp4'), 'video/mp4')
+    Assert.equal(Mime.get('file.txt'), 'text/plain')
+    Assert.equal(Mime.get('file.json'), 'application/json')
+    Assert.equal(Mime.get('file.xml'), 'application/xml')
   })
 
   it('should return application octet-stream for unknown', () => {
-    Assert.equal(Mime.lookup('file.unknown'), 'application/octet-stream')
+    Assert.equal(Mime.get('file.unknown'), 'application/octet-stream')
+  })
+
+  it('should set user defined mime type', () => {
+    Mime.set('.customext', 'custom/ext')
+    Assert.equal(Mime.get('app.customext'), 'custom/ext')
   })
 })
