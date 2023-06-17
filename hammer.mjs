72f42819a94f6f80e2f59ea68a67de4060e46bb7
sinclair <haydn.developer@gmail.com>
2023-06-17 10:26:56 +0900

    version

--- hammer.mjs
+++ hammer.mjs
@@ -4,7 +4,7 @@ import { compilePackage, packPackage } from './build/index'
 // Packages
 // -------------------------------------------------------------
 
-const version = '0.12.5'
+const version = '0.12.6'
 const packages = [
     ['async',     version, 'Sidewinder Async'],
     ['buffer',    version, 'Sidewinder Buffer'],
